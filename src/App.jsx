8e90a5f08b7397822d0db8fece081c63d30b1720
anikmondol <anikmondol558363@gmil.com>
2023-11-12 02:07:07 +0600

    create a list of components to create connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import ReusableForm from './components/ReusableForm/ReusableForm'
+import Grandpa from './components/Grandpa/Grandpa'
+// import ReusableForm from './components/ReusableForm/ReusableForm'
 // import HookForm from './components/HookForm/HookForm'
 // import RefForm from './components/RefForm/RefForm'
 // import StatefulForm from './components/StatefulForm/StatefulForm'
@@ -8,22 +9,23 @@ import ReusableForm from './components/ReusableForm/ReusableForm'
 function App() {
   
 
-  const handleSingUpSubmit = data => {
-    console.log('sign up data', data);
-}
-  const handUpdateProfile = data => {
-    console.log('update profile', data);
-}
+//   const handleSingUpSubmit = data => {
+//     console.log('sign up data', data);
+// }
+//   const handUpdateProfile = data => {
+//     console.log('update profile', data);
+// }
 
   return (
     <>
       
-      <h1>Vite + React</h1>
+      <h1>Form Master</h1>
+      <Grandpa></Grandpa>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
       {/* <HookForm></HookForm> */}
-      <ReusableForm formTitle={'Sing Up'} handleSubmit={handleSingUpSubmit}>
+      {/* <ReusableForm formTitle={'Sing Up'} handleSubmit={handleSingUpSubmit}>
         <div>
           <h2>Sing Up</h2>
           <p>please sing up right now</p>
@@ -34,7 +36,7 @@ function App() {
           <h2>Update Profile</h2>
           <p>Always keep your profile update</p>
         </div>
-      </ReusableForm>
+      </ReusableForm> */}
     </>
   )
 }
